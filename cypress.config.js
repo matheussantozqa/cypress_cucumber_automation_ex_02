350bddd943f4e04f1e19d475e0dbe50ce539d22c
matheussantozqa <matheussantoz.qa@gmail.com>
2026-02-21 17:54:52 -0300

    change commandTimeout

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.automationexercise.com",
-    defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 15000,
     viewportHeight: 900,
     viewportWidth: 1400,
     experimentalRunAllSpecs: true,
