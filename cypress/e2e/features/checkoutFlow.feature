c4d8b0ac4b540106899a6c9ddb8d3c0f8a12e8d4
matheussantozqa <matheussantoz.qa@gmail.com>
2026-02-21 16:28:53 -0300

    Feature and step-definition files finish

--- cypress/e2e/features/checkoutFlow.feature
+++ cypress/e2e/features/checkoutFlow.feature
@@ -18,7 +18,6 @@ When User completes signup form and creates account
 Then Account creation confirmation should appear
 Then User should be logged in and visible at header
 When User returns to cart page
-When User proceeds to checkout again
 Then Checkout page should display address details and order review
 When User types comment and confirms place order
 When User fills payment form with card details
@@ -27,37 +26,19 @@ Then Order success confirmation message should be displayed
 When User removes account through delete option
 Then Account deletion confirmation should be displayed
 
-
 @focus
 Scenario: Place Order - Login Before Checkout
 Given User opens application in browser
-When User navigates to application URL
 Then Home screen should load correctly
 When User opens authentication page
 When User logs in with valid credentials
 Then User login indicator should be visible
-When User adds items into shopping cart
-When User opens cart page
-Then Cart summary page should be visible
-When User proceeds to checkout
-Then Checkout page should display address details and order review
-When User types comment and confirms place order
-When User fills payment form with card details
-When User confirms payment submission
-Then Order success confirmation message should be displayed
+When User adds items to cart
+When User goes to cart page
+Then Cart summary should be visible
+When User goes to checkout
+Then Coment and place order
+When User fills payment details then clicks confirm
+Then Order success message should be displayed
 
 
-@focus
-Scenario: Add Multiple Products, Login and Place Order
-When User adds multiple products into cart
-When User opens cart page
-Then Cart summary page should be visible
-When User proceeds to checkout
-When User selects register or login option
-When User completes login form
-Then User authentication should be successful
-Then User navigates back to checkout page
-Then Checkout page should show populated address and order details
-When User types comment and confirms place order
-Then User confirms payment submission
-Then Order success confirmation message should be displayed
--- cypress/e2e/step-definitions/checkoutFlow.js
+++ cypress/e2e/step-definitions/checkoutFlow.js
@@ -10,11 +10,11 @@ Given("User accesses homepage", () => {
 });
 
 Then("User navigates to products listing page", () => {
-  cy.get("").click();
+  cy.get("[href='/products']").click();
 });
 
 Then("Products listing screen should be displayed", () => {
-  cy.url().should("include", "");
+  cy.contains("Category").should("be.visible");
 });
 
 
@@ -23,80 +23,111 @@ Then("Products listing screen should be displayed", () => {
 // ============================
 
 When("User adds items into shopping cart", () => {
-  cy.get("").each(($el, index) => {
-    if (index < 2) {
-      cy.wrap($el).trigger("mouseover");
-      cy.wrap($el).click();
-    }
-  });
+  cy.get("[data-product-id='4']").eq(0).click();
+  cy.get("[data-dismiss='modal']").click();
+  cy.get("[data-product-id='6']").eq(0).click();
 });
 
 When("User opens cart page", () => {
-  cy.get("").click();
+  cy.get("[href='/view_cart']").eq(0).click();
 });
 
 Then("Cart summary page should be visible", () => {
-  cy.url().should("include", "");
+  cy.url().should("be.equal", Cypress.config().baseUrl + "/view_cart");
 });
 
 When("User proceeds to checkout", () => {
-  cy.get("").click();
+  cy.get(".check_out").click();
 });
 
 When("User selects register or login option", () => {
-  cy.get("").click();
+  cy.get("[href='/login']").eq(1).click();
 });
 
 When("User completes signup form and creates account", () => {
-  cy.get("").type(""); // name
-  cy.get("").type(""); // email
-  cy.get("").type(""); // password
-  cy.get("").click();  // submit signup
+
+  cy.get("[placeholder='Name']").type("Joselito");  
+  cy.get("[placeholder='Email Address']").eq(1).type("joselitocabron@gmail.com"); 
+  cy.get("[data-qa='signup-button']").click();
+
+  cy.get('body').then(($body) => {
+    if ($body.text().includes('Email Address already exist!')) {
+      cy.get("[data-qa='login-email']").type("joselitocabron@gmail.com"); 
+      cy.get("[data-qa='login-password']").type("45678test"); 
+      cy.get("[data-qa='login-button']").click();
+      cy.get("[href='/delete_account']").click();
+      cy.get("[placeholder='Name']").type("Joselito");  
+      cy.get("[placeholder='Email Address']").eq(1).type("joselitocabron@gmail.com"); 
+      cy.get("[data-qa='signup-button']").click();
+    }
+});
+  cy.get("[value='Mr']").click();
+  cy.get("[data-qa='password']").type("45678test"); 
+  cy.get("[data-qa='days']").select("6"); 
+  cy.get("[data-qa='months']").select("May"); 
+  cy.get("[data-qa='years']").select("1980"); 
+  cy.get("[name='newsletter']").check();
+  cy.get("[name='optin']").check();
+
+  cy.get("[data-qa='first_name']").type("Joselito");
+  cy.get("[data-qa='last_name']").type("Cabron");
+  cy.get("[data-qa='company']").type("Farmer");
+  cy.get("[data-qa='address']").type("San Juan, 5234 - Xito");
+  cy.get("[data-qa='country']").select("Israel");
+  cy.get("[data-qa='state']").type("Cali");
+  cy.get("[data-qa='city']").type("Navidad");
+  cy.get("[data-qa='zipcode']").type("5352356");
+  cy.get("[data-qa='mobile_number']").type("4356867954");
 });
 
 Then("Account creation confirmation should appear", () => {
-  cy.contains("").should("be.visible");
+  cy.get("[data-qa='create-account']").click();
+  cy.get("[data-qa='continue-button']").click();
 });
 
 Then("User should be logged in and visible at header", () => {
-  cy.get("").should("be.visible");
+  cy.contains("Logged in as").should("be.visible");
 });
 
 When("User returns to cart page", () => {
-  cy.get("").click();
+  cy.get("[href='/view_cart']").eq(0).click();
+  cy.get(".check_out").click();
 });
 
 Then("Checkout page should display address details and order review", () => {
-  cy.get("").should("be.visible"); 
-  cy.get("").should("be.visible"); 
+  cy.contains("Your delivery address").should("be.visible"); 
+  cy.contains("Your billing address").should("be.visible");
 });
 
 When("User types comment and confirms place order", () => {
-  cy.get("").type(""); // comment textarea
-  cy.get("").click();  // place order button
+  cy.get("[name='message']").type("This is a test order comment"); 
+  cy.get("[href='/payment']").click();  
 });
 
 When("User fills payment form with card details", () => {
-  cy.get("").type(""); // name on card
-  cy.get("").type(""); // card number
-  cy.get("").type(""); // CVC
-  cy.get("").type(""); // expiration
+  cy.get("[name='name_on_card']").type("Joselito C"); 
+  cy.get("[data-qa='card-number']").type("14356242452"); 
+  cy.get("[data-qa='cvc']").type("492"); 
+  cy.get("[data-qa='expiry-month']").type("12/30"); 
+  cy.get("[data-qa='expiry-year']").type("2034");
 });
 
 When("User confirms payment submission", () => {
-  cy.get("").click();
+  cy.get("[data-qa='pay-button']").click();
 });
 
 Then("Order success confirmation message should be displayed", () => {
-  cy.contains("").should("be.visible");
+  cy.get("[style='font-size: 20px; font-family: garamond;']").should("contain.text", "Congratulations! Your order has been confirmed!");
+  cy.get("[data-qa='order-placed']").should("be.visible");
 });
 
 When("User removes account through delete option", () => {
-  cy.get("").click();
+  cy.get("[href='/delete_account']").click();
 });
 
 Then("Account deletion confirmation should be displayed", () => {
-  cy.contains("").should("be.visible");
+  cy.get("[data-qa='account-deleted']").should("be.visible");
+  cy.contains("Your account has been permanently deleted!").should("contain.text", "Your account has been permanently deleted!");
 });
 
 
@@ -108,54 +139,67 @@ Given("User opens application in browser", () => {
   cy.visit("");
 });
 
-When("User navigates to application URL", () => {
-  cy.visit("");
-});
-
 Then("Home screen should load correctly", () => {
-  cy.get("").should("be.visible");
+  cy.get(".title").eq(0).should("be.visible");
 });
 
 When("User opens authentication page", () => {
-  cy.get("").click();
+  cy.get("[href='/login']").click();
 });
 
 When("User logs in with valid credentials", () => {
-  cy.get("").type(""); // email
-  cy.get("").type(""); // password
-  cy.get("").click();  // login
+  cy.get("[data-qa='login-email']").type("Antoieine@test.com");
+  cy.get("[data-qa='login-password']").type("12345"); 
+  cy.get("[data-qa='login-button']").click();
 });
 
 Then("User login indicator should be visible", () => {
-  cy.get("").should("be.visible");
+  cy.contains("Logged in as").should("be.visible");
+});
+
+When(`User adds items to cart`, () => {
+  cy.get("[data-product-id='3']").eq(0).click();
+  cy.get("[data-dismiss='modal']").click();
+  cy.get("[data-product-id='13']").eq(0).click();
+  cy.get("[data-dismiss='modal']").click();
+  cy.get("[data-product-id='18']").eq(0).click();
+  cy.get("[data-dismiss='modal']").click();
+  cy.get("[data-product-id='21']").eq(0).click();
+  cy.get("[data-dismiss='modal']").click();
+  cy.get("[data-product-id='42']").eq(0).click();
+});
+
+When(`User goes to cart page`, () => {
+  cy.get("[href='/view_cart']").eq(1).click();
+});
+
+Then(`Cart summary should be visible`, () => {
+  cy.contains("Item").should("be.visible");
+  cy.contains("Description").should("be.visible");
+});
+
+When(`User goes to checkout`, () => {
+  cy.get(".check_out").click();
+});
+
+Then('Coment and place order', () => {
+  cy.get("[name='message']").type("Thanks for everything, this is a test order comment"); 
+  cy.get("[href='/payment']").click(); 
+});
+
+When(`User fills payment details then clicks confirm`, () => {
+  cy.get("[name='name_on_card']").type("Antoine C"); 
+  cy.get("[data-qa='card-number']").type("143568762"); 
+  cy.get("[data-qa='cvc']").type("452"); 
+  cy.get("[data-qa='expiry-month']").type("05/26"); 
+  cy.get("[data-qa='expiry-year']").type("2028");   
+  cy.get("[data-qa='pay-button']").click();
+});
+
+Then(`Order success message should be displayed`, () => {
+  cy.get("[style='font-size: 20px; font-family: garamond;']").should("contain.text", "Congratulations! Your order has been confirmed!");
+  cy.get("[data-qa='order-placed']").should("be.visible"); 
+  cy.get("[href='/logout']").click(); 
 });
 
 
-// ============================
-// Add Multiple Products, Login and Place Order
-// ============================
-
-When("User adds multiple products into cart", () => {
-  cy.get("").each(($el) => {
-    cy.wrap($el).trigger("mouseover");
-    cy.wrap($el).click();
-  });
-});
-
-When("User completes login form", () => {
-  cy.get("").type(""); // email
-  cy.get("").type(""); // password
-  cy.get("").click();
-});
-
-Then("User authentication should be successful", () => {
-  cy.get("").should("be.visible");
-});
-
-Then("User navigates back to checkout page", () => {
-  cy.get("").click();
-});
-
-Then("Checkout page should show populated address and order details", () => {
-  cy.get("").should("be.visible");
-});
